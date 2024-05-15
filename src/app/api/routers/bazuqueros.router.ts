d94abc4dab0c85f6b3456199627a337bba41a810
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-05-15 08:44:59 -0500

    Update bazuqueros.router.ts

--- src/app/api/routers/bazuqueros.router.ts
+++ src/app/api/routers/bazuqueros.router.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { BASE_URL } from '../axios';
+import { BASE_URL } from '@/app/api/axios.ts';
 
 export const getLosBazuquerosResourcesRequest = async () => {
   const res = await axios.get(BASE_URL + '/clienteintegracion')
